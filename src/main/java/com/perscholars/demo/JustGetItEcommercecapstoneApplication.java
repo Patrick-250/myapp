32ed47758c50184f5ffdcfe238e0e7a2e79748d4
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2023-03-06 11:05:18 -0800

    init commit for project Ecommerce

---
+++ src/main/java/com/perscholars/demo/JustGetItEcommercecapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.perscholars.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGetItEcommercecapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGetItEcommercecapstoneApplication.class, args);
+	}
+
+}
